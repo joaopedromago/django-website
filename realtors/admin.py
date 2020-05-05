2b02e9fb5fcbc8ca87bde8dabc4a698ece2e64d0
Mago <mago@localhost.localdomain>
2020-05-05 13:48:56 -0300

    feat: changing admin list configuration

--- realtors/admin.py
+++ realtors/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 
 from .models import Realtor
 
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'hire_date')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
 
-admin.site.register(Realtor)
+admin.site.register(Realtor, RealtorAdmin)
