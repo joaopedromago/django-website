ddd431048fbffe447ad483df210bb22d540f7721
Mago <mago@localhost.localdomain>
2020-05-06 15:31:39 -0300

    feat: add functions to load data from view

--- listings/views.py
+++ listings/views.py
@@ -1,12 +1,26 @@
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Listing
+
 
 def index(request):
-    return render(request, 'listings/listings.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
 
-def listing(request):
+    paginator = Paginator(listings, 6)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+
+    context = {
+        'listings': paged_listings
+    }
+    return render(request, 'listings/listings.html', context)
+
+
+def listing(request, listing_id):
     return render(request, 'listings/listing.html')
+
 
 def search(request):
     return render(request, 'listings/search.html')
