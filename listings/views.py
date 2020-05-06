30aef669c3f731dfcf796002edce69a7f849ba0a
Mago <mago@localhost.localdomain>
2020-05-06 17:33:02 -0300

    feat: linking listings and homepage with api

--- listings/views.py
+++ listings/views.py
@@ -1,9 +1,8 @@
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Listing
-
 
 def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)
@@ -19,7 +18,12 @@ def index(request):
 
 
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html')
+    listing = get_object_or_404(Listing, pk=listing_id)
+
+    context = {
+        'listing': listing
+    }
+    return render(request, 'listings/listing.html', context)
 
 
 def search(request):
